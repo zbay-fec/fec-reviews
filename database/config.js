b8270551f80be3efdd14ec936565717190f5fb55
Eisenbartl <Eisenbartl@yahoo.com>
2019-07-17 17:28:02 -0500

    rebundle

--- database/config.js
+++ database/config.js
@@ -4,6 +4,5 @@ module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
+    database: process.env.DB_DATABASE
 }
