5f4cd6535526e71becb1e71e3dc12663c1066f81
Eisenbartl <Eisenbartl@yahoo.com>
2019-07-17 10:47:54 -0500

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-18-188-142-187.us-east-2.compute.amazonaws.com',
+        host: 'ec2-18-224-212-185.us-east-2.compute.amazonaws.com',
         key: '~/.ssh/tutorial.pem',
         ref: 'origin/master',
         repo: 'git@github.com:zbay-fec/fec-reviews.git',
