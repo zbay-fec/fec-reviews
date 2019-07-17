52fd222880a6af5833429082eaf93c7579279e09
Eisenbartl <Eisenbartl@yahoo.com>
2019-07-17 10:38:24 -0500

    updated

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+      name: 'reviews',
+      script: './server/server.js'
+    }],
+    deploy: {
+      production: {
+        user: 'ubuntu',
+        host: 'ec2-18-188-142-187.us-east-2.compute.amazonaws.com',
+        key: '~/.ssh/tutorial.pem',
+        ref: 'origin/master',
+        repo: 'git@github.com:zbay-fec/fec-reviews.git',
+        path: '/home/ubuntu/fec-reviews',
+        'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      }
+    }
+  }
