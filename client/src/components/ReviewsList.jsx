9cbd2f94d2ad19d46970f7b17fc4875957ca4897
Eisenbartl <Eisenbartl@yahoo.com>
2019-08-20 08:56:38 -0500

    post request update

--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -96,8 +96,8 @@ class ReviewsList extends React.Component {
                 review_text: this.state.reviewInput
             }
             // console.log(newReview);
-            // axios.post('http://ec2-52-14-196-245.us-east-2.compute.amazonaws.com:3003/newReviews', { newReview })
-            axios.post('http://localhost:3003/newReviews', { newReview })
+            axios.post('http://ec2-18-191-134-3.us-east-2.compute.amazonaws.com:3003/newReviews', { newReview })
+            // axios.post('http://localhost:3003/newReviews', { newReview })
                 .then( res => this.onChange())
                 .catch( err => console.log('Error, ', err))
         }
